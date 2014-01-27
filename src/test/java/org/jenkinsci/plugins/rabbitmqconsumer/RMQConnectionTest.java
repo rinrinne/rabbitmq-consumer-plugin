442c9680f01b8b34fdab1ceb24e11f9f33a1e079
rinrinne <rinrin.ne@gmail.com>
2014-01-27 19:15:31 +0900

    Small fix for RMQConnectionTest

--- src/test/java/org/jenkinsci/plugins/rabbitmqconsumer/RMQConnectionTest.java
+++ src/test/java/org/jenkinsci/plugins/rabbitmqconsumer/RMQConnectionTest.java
@@ -42,7 +42,7 @@ public class RMQConnectionTest {
     ReconnectTimer timer = new ReconnectTimer();
 
     @Mocked
-    MessageQueueListener mqListener;
+    MessageQueueListener mqListener = null;
 
     Connection connection;
     Channel channel;
